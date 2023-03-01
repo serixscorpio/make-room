c33fd66f53bde77b66793c4c4e3c53dabe4a2086
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-03-01 11:12:40 +0800

    Add pyfakefs to facilitate testing using a fake fs.

--- tests/test_make_room.py
+++ tests/test_make_room.py
@@ -1,1 +1,8 @@
 import pytest
+import os
+
+
+def test_my_fakefs(fs):
+    # "fs" is the reference to the fake file system
+    fs.create_file("/var/data/xx1.txt")
+    assert os.path.exists("/var/data/xx1.txt")
