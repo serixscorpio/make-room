4c753d9b1c89f2f1b946cc909936181c515f7e9c
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-05-26 15:46:30 +0800

    Remove noxfile.
    Since we now rely on hatch environments, there is no longer a need to
    use nox to manage a separate environment just for running pre-commit.

--- noxfile.py
+++
@@ -1,9 +0,0 @@
-from nox_poetry import Session, session
-
-
-@session(name="pre-commit", python="3.10")
-def precommit(session: Session) -> None:
-    """Lint using pre-commit."""
-    args = session.posargs or ["run", "--all-files", "--show-diff-on-failure"]
-    session.install("bandit", "black", "pre-commit", "pre-commit-hooks")
-    session.run("pre-commit", *args)
