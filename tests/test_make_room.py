6dfe85c04bd718cf3700ca9cbcc29a460db63cba
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2023-05-30 11:56:31 +0800

    Rename test video name.
    Also remove unused test videos.

--- tests/test_make_room.py
+++ tests/test_make_room.py
@@ -11,7 +11,10 @@ fixture_path = os.path.join(os.path.dirname(__file__), "fixtures")
 def test_make_room():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        shutil.copy(f"{fixture_path}/original.mp4", ".")
+        shutil.copy(f"{fixture_path}/example-video.mp4", ".")
         runner.invoke(make_room.main, ["."], catch_exceptions=False)
-        assert make_room.encoded_with_crf("original-c.mp4")
-        assert os.stat("original.mp4").st_size > os.stat("original-c.mp4").st_size
+        assert make_room.encoded_with_crf("example-video-c.mp4")
+        assert (
+            os.stat("example-video.mp4").st_size
+            > os.stat("example-video-c.mp4").st_size
+        )
